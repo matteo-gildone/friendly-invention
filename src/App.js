686760502cbb62a64ca67030a92bcde9328d7ca7
Matteo Gildone <matteo.gildone@springernature.com>
2022-01-21 15:55:04 +0000

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  );
+    return (
+        <div className={"container mx-auto"}>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
